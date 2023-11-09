0877eecc38a2145c17688b440b247126e3d9aa26
Pavlov Sergey <pavlov_sv3@nlmk.com>
2023-11-09 14:37:23 +0300

    stable version

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import { Login } from "./src/pages/login";
-import { SignIn } from "./src/pages/signin";
-import { Profile } from "./src/pages/profile";
 import Router from "./src/routing/Router";
 import { StoreApp } from "./src/core/Store";
 import { getUserInfo } from "./src/controllers/AuthController";
 import { Error400 } from "./src/pages/Error400/index";
 import { Error500 } from "./src/pages/Error500/index";
 import { Dialog } from "./src/pages/Dialog/index";
+import { Profile } from "./src/pages/Profile";
+import { Login } from "./src/pages/Login";
+import { SignIn } from "./src/pages/SignIn";
 
 document.addEventListener("DOMContentLoaded", async () => {
   Router.use("/", Login);
