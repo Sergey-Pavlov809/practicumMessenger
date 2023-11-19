f8c1d663596e0dafba0b253317144a8dff2b6709
Pavlov Sergey <pavlov_sv3@nlmk.com>
2023-11-19 18:18:36 +0300

    test precommit

---
+++ src/components/LinkButton/LinkButton.test.ts
@@ -0,0 +1,16 @@
+import sinon from "sinon";
+import { LinkButton } from ".";
+import { expect } from "chai";
+
+describe("LinkButton", () => {
+  it("Должен быть кликабельным", () => {
+    const callback = sinon.stub();
+    const linkButton = new LinkButton({ events: { click: callback } });
+  
+    const element = linkButton.element as HTMLElement;
+  
+    element.click();
+  
+    expect(callback.calledOnce).to.eq(true);
+  });
+});
